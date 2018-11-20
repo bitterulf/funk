65e2904db4e5b6e3e4f8fd71087ff7496228ac95
bitterulf <bitterulf@users.noreply.github.com>
2018-11-20 12:56:03 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const Joi = require('joi');
+const HapiSwagger = require('hapi-swagger');
+const Pack = require('./package');
+const FS = require('fs');
+const Util = require('util');
+const exec = Util.promisify(require('child_process').exec);
+
+const server=Hapi.server({
+    host:'localhost',
+    port:8080
+});
+
+const helloHandler = async function(request, h) {
+
+    return h.response('hello you');
+}
+
+server.route({
+    method:'GET',
+    path:'/hello',
+    options: {
+        handler: helloHandler,
+        description: 'hello',
+        notes: 'hello',
+        tags: ['api']
+    }
+});
+
+const swaggerOptions = {
+    info: {
+        title: 'Test API Documentation',
+        version: Pack.version,
+    },
+};
+
+async function start() {
+    await server.register([
+        Inert,
+        Vision,
+        {
+            plugin: HapiSwagger,
+            options: swaggerOptions
+        }
+    ]);
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
